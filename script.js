d6bbe6e4be681416f38fb8b7bd56b186f0d80a04
iqwixn <aleksey.nizhnikov@gmail.com>
2023-07-05 17:50:10 -0700

    added password length to javascript code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", "~", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
+var length;
 
 
 // Write password to the #password input
@@ -19,25 +21,39 @@ function writePassword() {
 // Password will not generate if under 8 characters or over 128 characters.
 // If all conditions are met, password will generate as an alert or written to the page.
 
-function genLowercase() {
-  return lowerChars[Math.floor(Math.random() * lowerChars.length)];
-
-}
-
-function genUppercase() {
-  return upperChars[Math.floor(Math.random() * upperChars.length)];
-
-}
-
-function genNumber() {
-  return numbers[Math.floor(Math.random() * numbers.length)];
-
-}
-
 function generatePassword() {
-    console.log("test");
-    return numbers[Math.floor(Math.random() * numbers.length)];
+  length = window.prompt ("Please enter a number between 8 and 128");
+  
+  if (!length){
+    window.alert("Please choose a number between 8 and 128")
+  }
+
+  else if (length < 8 || length > 128){
+    window.confirm("Please choose a number between 8 and 128 ");
+   
+  }
+
 }
+
+//function genLowercase() {
+ // return lowerChars[Math.floor(Math.random() * lowerChars.length)];
+
+//}
+
+//function genUppercase() {
+//  return upperChars[Math.floor(Math.random() * upperChars.length)];
+
+//}
+
+//function genNumber() {
+ // return numbers[Math.floor(Math.random() * numbers.length)];
+
+//}
+
+//function generatePassword() {
+  //  console.log("test");
+  //  return numbers[Math.floor(Math.random() * numbers.length)];
+//}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
